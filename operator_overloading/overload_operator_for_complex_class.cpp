a69a16d8b6bae0dd2e8f15e806d519b9577f11a2
Abhilekh Gautam <abhilekhgautam119@gmail.com>
2021-06-28 15:51:37 +0545

    Improved indentation

--- operator_overloading/overload_operator_for_complex_class.cpp
+++ operator_overloading/overload_operator_for_complex_class.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 class Complex{
-int real,imag;
-public:
-Complex(int re,int im):real(re),imag(im){}
-Complex(){
-real = 0;
-imag = 0;
+  int real,imag;
+ public:
+  Complex(int re,int im):real(re),imag(im){}
+  Complex(){
+      real = 0;
+      imag = 0;
 }
 //overloaded operators
 Complex operator+(const Complex);
@@ -19,30 +19,30 @@ friend ostream& operator<<(ostream&,Complex);
 };
 
 ostream& operator<<(ostream& os,Complex c1){
-if(c1.imag<0)
-os<<c1.real<<c1.imag<<"i";
-else
-os<<c1.real<<"+"<<c1.imag<<"i";
-return os;
+    if(c1.imag<0)
+      os<<c1.real<<c1.imag<<"i";
+    else
+      os<<c1.real<<"+"<<c1.imag<<"i";
+    return os;
 }
 //overloading + operator
 Complex Complex::operator+(const Complex c1){
-Complex temp;
-temp.real = real + c1.real;
-temp.imag = imag + c1.imag;
-return temp;
+    Complex temp;
+    temp.real = real + c1.real;
+    temp.imag = imag + c1.imag;
+    return temp;
 }
 // overloading - operator
 Complex Complex::operator-(const Complex c1){
-Complex temp;
-temp.real = real - c1.real;
-temp.imag = imag - c1.imag;
-return temp;
+    Complex temp;
+    temp.real = real - c1.real;
+    temp.imag = imag - c1.imag;
+    return temp;
 }
 //overloading assignment operator
 void Complex::operator=(const Complex c1){
-real = c1.real;
-imag = c1.imag;
+    real = c1.real;
+    imag = c1.imag;
 }
 //overloading != operator
 bool Complex::operator!=(const Complex c1){
@@ -61,10 +61,10 @@ bool Complex::operator==(const Complex c1){
     return false;
 }
 int main(){
-Complex c1(2,2);
-Complex c2(2,2);
+    Complex c1(2,2);
+    Complex c2(2,2);
 
-cout<<c1<<'\n';
-cout<<c2<<'\n';
+    cout<<c1<<'\n';
+    cout<<c2<<'\n';
 }
 
