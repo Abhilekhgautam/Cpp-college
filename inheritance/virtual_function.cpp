8ddde9506f3b5c38d6039cf6eda7fd693bd82988
Abhilekh Gautam <abhilekhgautam119@gmail.com>
2021-06-29 15:29:16 +0545

    Minor change

--- inheritance/virtual_function.cpp
+++ inheritance/virtual_function.cpp
@@ -1,45 +1,87 @@
 //Demonstration of a virtual function
-#include<iostream>
-using namespace std;
+/*
+#include <iostream>
+#include <string>
+
 
 class Employee{
-    string first_name,family_name;
+    std::string first_name,family_name;
     int department;
     public:
-    virtual void print();
+       void print(){
+        std::cout<<"Hello from print function of Employee Class"<<'\n';
+      }
 };
 
 class Manager:public Employee{
     public:
-    void print();
+       void print(){
+         std::cout<<"Hello from print function of Manager Class"<<'\n';
+    }
 };
 
 class Prof:public Employee{
     public:
-    void print();
+     void print(){
+        std::cout<<"Hello from print function of Prof Class"<<'\n';
+    }
 };
 
-void Employee::print (){
-    cout<<"Hello from print function of Employee Class"<<'\n';
-}
-
-void Manager::print (){
-    cout<<"Hello from print function of Manager Class"<<'\n';
-}
-
-void Prof::print(){
-    cout<<"Hello from print function of Prof Class"<<'\n';
-
-}
-
 int main(){
+    //pointer to base type
     Employee* obj;
     Manager objMan;
     Prof objprof;
 
     obj = &objMan;
-    obj->print();
+    obj->print();// calls the function of Employee class
     obj = &objprof;
-    obj->print();
+    obj->print();// calls the function of Employee class
+    return 0;
+}
+*/
+
+
+/*
+Using virtual function allows us to override the base definition of that function 
+in the derived class.
+*/
+
+#include<iostream>
+
+class Employee{
+   std::string first_name,family_name;
+    int department;
+    public:
+    //virtual function:can be overidden in derived class
+      virtual void print(){
+        std::cout<<"Hello from print function of Employee Class"<<'\n';
+      }
+};
+
+class Manager:public Employee{
+    public:
+       void print(){
+         std::cout<<"Hello from print function of Manager Class"<<'\n';
+    }
+};
+
+class Prof:public Employee{
+    public:
+     void print(){
+        std::cout<<"Hello from print function of Prof Class"<<'\n';
+    }
+};
+
+int main(){
+    //pointer to base type
+    Employee* obj;
+    Manager objMan;
+    Prof objprof;
+
+    obj = &objMan;
+    obj->print(); // calls the function of Manager Class
+    obj = &objprof;
+    obj->print(); // calls the function of Prof Class
     return 0;
 }
